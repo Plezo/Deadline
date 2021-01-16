055ee4a410dcb62c3b7e44d7308f172027282370
Plezo <Plezo@protonmail.com>
2021-01-15 23:38:57 -0500

    Changed TODO

--- commands/deadline.js
+++ commands/deadline.js
@@ -3,17 +3,6 @@ TODO:
 -Figure out a way to only have one arg be passed through each func, no msg arg,
 probably have each function return a string where the main module.exports func
 will output it, instead of having to create a new embed per function
--Consider changing view function to group deadlines by due dates
--Consider having an "edit" function, instead of creating a new array for each subject,
-we can have it so we can safely assume the subjects that NEED to be in the list and if
-someone tries adding a deadline for a subject not in the list then returns "Subject not
-in config" or something
--The view deadlines function will have you specify the subject you want to view,
-if you do not type the specific subject, it will give you a list of all the subjects
--Consider adding a description argument for each deadline that may be optional,
-it may overcomplicate things tho so maybe stick to just assignment and due
--For due date, do something like d/m|h:min where the '|' is the delimiter, if a '|' does
-not exist then just make the default time 11:59:59
 -Delete each message the bot sends after x amount of seconds
 */
 
